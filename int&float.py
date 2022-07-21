19790ca4b1335888d301640e2d7b1b9b868aa4c1
priyamohan08 <109510877+priyamohan08@users.noreply.github.com>
2022-07-21 13:51:58 +0530

    Add files via upload

---
+++ int&float.py
@@ -0,0 +1,31 @@
+# a=456
+# print(type(a))
+# b=65.09
+# print(type(b))
+# c=6.876
+# print(type(c))
+# print(type(3748524657823432))
+# a=6  ;b=9         
+# print(a)
+# print('%d'%b)
+
+# e=7.234
+# print(e)
+# print('%d'%e)
+# l=9.876
+# print('%.1f'%1)
+# print('%,2f'%1)
+# print('%.3f'%1)
+
+
+'''Type of conversion &casting'''
+# m=6547
+# print(tyoe(m))
+# print(float(m))
+# print('%d'%m)
+# print('%f'%m)
+n=3.98
+# print(type(n))
+# print(int(n))
+# print('%d'%n)
+# print('%f'%)
